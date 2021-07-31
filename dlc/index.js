87da92bfe941cdb72222fd5a0015323b0fbedbec
Zachary Waldowski <zach@waldowski.me>
2021-07-31 14:41:58 -0400

    DAVID G!

--- dlc/index.js
+++ dlc/index.js
@@ -1,13 +1,13 @@
 [
-    { 
-        "file": "context.dlc", 
+    {
+        "file": "context.dlc",
         "title": "Context Logo",
         "buttons": [
             { "title": "Logo", "action": [75,0,4,4] }
         ]
     },
-    { 
-        "file": "hacked.dlc", 
+    {
+        "file": "hacked.dlc",
         "title": "HACKED",
         "buttons": [
             { "title": "Hacked 1", "action": [75,0,3,4] },
@@ -15,24 +15,31 @@
         ]
     },
     {
-        "file": "noimg2.dlc", 
+        "file": "noimg2.dlc",
         "title": "Audio only",
         "buttons": [
             { "title": "Audio test ", "action": [75,0,4,4] }
         ]
     },
     {
-        "file": "chilli.dlc", 
+        "file": "chilli.dlc",
         "title": "Chilli",
         "buttons": [
             { "title": "Chilli ", "action": [75,0,4,4] }
         ]
     },
     {
-        "file": "parkupb.dlc", 
+        "file": "parkupb.dlc",
         "title": "parkupb",
         "buttons": [
             { "title": "parkupb", "action": [75,0,0,3] }
+        ]
+    },
+    {
+        "file": "newdlc.dlc",
+        "title": "David G",
+        "buttons": [
+            { "title": "newdlc", "action": [76,0,0,16] }
         ]
     }
 ]
